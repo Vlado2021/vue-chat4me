4b962796a70fc0abb34288fc8ad75223bf1e48ba
Vlado2021 <92333488+Vlado2021@users.noreply.github.com>
2024-02-25 13:04:20 +0200

    my first changes 8888888888888888

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,48 @@
 <template>
- <div class="app">
-  <form @submit.prevent >
-    <h4>Создание поста</h4>
-    <input 
-    name="title1"
-    v-bind:value="title" 
-    @input="title=$event.target.value;" 
-    class="input" 
-    type="text" 
-    placeholder="Название">
-    <input 
-    name="body1"
-     v-bind:value="body" 
-     @input="body=$event.target.value;"
-     class="input" 
-     type="text" 
-     placeholder="Описание">
-    <button class="btn" v-on:click="createPost">Создать</button>
-  </form>
-  <div class="post" v-for="post in posts"> 
-    <div><strong>Название: </strong>{{ post.title }} </div>
-    <div><strong>Описание: </strong>{{ post.body }} </div>
-  </div>
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input name="title1" v-bind:value="title" @input="title = $event.target.value;" class="input" type="text"
+        placeholder="Название">
+      <input v-bind:value="body" @input="body = $event.target.value;" class="input" type="text" placeholder="Описание">
+      <button class="btn" v-on:click="createPost">Создать</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название: </strong>{{ post.title }} </div>
+      <div><strong>Описание: </strong>{{ post.body }} </div>
+    </div>
 
- </div>
+  </div>
 </template>
 
 <script >
 
 export default {
-  data(){
+  data() {
     return {
       posts: [
-        {id:  1, title: 'JavaScript', body: 'Описание поста'},
-        {id:  2, title: 'JavaScript2', body: 'Описание поста2'},
-        {id:  3, title: 'JavaScript3', body: 'Описание поста3'}
+        { id: 1, title: 'JavaScript', body: 'Описание поста' },
+        { id: 2, title: 'JavaScript2', body: 'Описание поста2' },
+        { id: 3, title: 'JavaScript3', body: 'Описание поста3' }
       ],
 
-      title:'',
-      body:''
+      title: '',
+      body: ''
 
     }
   },
   methods: {
-    createPost(){
-          const newPost={
-          id: Date.now(),
-          title: this.title,
-          body: this.body,
-        }
-           
-        this.posts.push(newPost);
-        },
-     
-  
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      }
+
+      this.posts.push(newPost);
+    },
+
+
   }
 
 }
@@ -61,17 +50,17 @@ export default {
 </script>
 
 <style >
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
-.app{
+.app {
   padding: 20px;
 }
 
-form{
+form {
   display: flex;
   flex-direction: column;
 }
@@ -99,5 +88,4 @@ form{
   border-radius: 5px;
   background-color: rgb(155, 219, 155);
 }
-
 </style>
