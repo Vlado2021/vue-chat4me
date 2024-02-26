3eac242f7af742d01fca64e83c74ef7b70155904
Vlado2021 <92333488+Vlado2021@users.noreply.github.com>
2024-02-26 18:38:50 +0200

    20240226 18:39 start to work with new branch

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,10 +1,8 @@
 <template>
   <form @submit.prevent>
     <h4>Создание поста</h4>
-    <input v-bind:value="post.title" @input="post.title = $event.target.value;" class="input" type="text"
-      placeholder="Название">
-    <input v-bind:value="post.body" @input="post.body = $event.target.value;" class="input" type="text"
-      placeholder="Описание">
+    <input v-model="post.title" class="input" type="text" placeholder="Название">
+    <input v-model="post.body" class="input" type="text" placeholder="Описание">
     <button class="btn" v-on:click="createPost">Создать</button>
   </form>
 </template>
