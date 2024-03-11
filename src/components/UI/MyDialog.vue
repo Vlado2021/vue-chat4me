3ff92aecfef48821f0867da1bfdd8ba197dadbc9
Vlado2021 <92333488+Vlado2021@users.noreply.github.com>
2024-03-11 17:47:47 +0200

    ddd

---
+++ src/components/UI/MyDialog.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="dialog" v-if="show===true"> </div>
+</template>
+    
+<script>
+    export default {
+     name:'my-dialog'
+     props: {
+         show: {
+             type: Boolean,
+             default: false
+         }
+     } 
+    }
+</script>
+    
+       
+  <style scoped>
+     
+    
+  </style>
