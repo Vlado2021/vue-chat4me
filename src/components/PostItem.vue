71accdefd5c51768bd07a8561c02606403dae3fa
Vlado2021 <92333488+Vlado2021@users.noreply.github.com>
2024-03-26 10:23:21 +0200

    20240326 10:23 Dynamic routing worked!

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -6,7 +6,7 @@
       <div><strong>Описание: </strong>{{ post5.body }} </div>
     </div>
     <div class="post__btns">
-      <my-button @click="$router.push('/posts/${post.id}')" >Открыть</my-button>  
+      <my-button @click="$router.push(`/posts/${post5.id}`)" >Открыть</my-button>  
       <my-button @click="$emit('remove', post)" >Удалить</my-button>  
     </div>
     </div>
